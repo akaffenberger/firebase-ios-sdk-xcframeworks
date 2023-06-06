12267ccc014ba6541b35b02ed0c08323743857bf
Mathijs Bernson <mathijsb@q42.nl>
2023-06-06 12:16:37 +0200

    Update repository reference

--- .scripts/package.sh
+++ .scripts/package.sh
@@ -274,7 +274,7 @@ set -o pipefail
 
 # Repos
 firebase_repo="https://github.com/firebase/firebase-ios-sdk"
-xcframeworks_repo="https://github.com/akaffenberger/firebase-ios-sdk-xcframeworks"
+xcframeworks_repo="https://github.com/Q42/firebase-ios-sdk-xcframeworks"
 
 # Release versions
 latest=$(latest_release_number $firebase_repo)
